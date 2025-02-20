cd8a5e000d2367dbf902f1185bcd32a32d8816b6
Imaniac230 <44968160+Imaniac230@users.noreply.github.com>
2025-02-20 14:44:10 +0100

    fix: Update robot config with latest changes, remove unnecessary ros dependencies, update clang-format with official humble rules.
    Signed-off-by: Imaniac230 <44968160+Imaniac230@users.noreply.github.com>

--- include/mpc-rbt-simulator/RobotConfig.hpp
+++ include/mpc-rbt-simulator/RobotConfig.hpp
@@ -2,7 +2,7 @@
 #define ROBOTCONFIG_HPP
 
 namespace robot_config {
-    static constexpr float HALF_DISTANCE_BETWEEN_WHEELS = 0.202;
+    static constexpr float HALF_DISTANCE_BETWEEN_WHEELS = 0.2022 + 0.00725;
     static constexpr float WHEEL_RADIUS = 0.0985;
 }// namespace robot_config
 
